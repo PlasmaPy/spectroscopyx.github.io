c1af6de2da6ad608ac242064339a9bc90e33f25a
Pawel Marek Kozlowski <pawel@pmkozlowski.com>
2018-05-02 16:01:00 -0600

    Update site to plasmapy.github.io/spectroscopyx instead of spectroscopyx.github.io

--- web/conf.py
+++ web/conf.py
@@ -21,10 +21,10 @@ BLOG_AUTHOR = "SpectroscoPyx Developers"  # (translatable)
 BLOG_TITLE = "SpectroscoPyx"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://spectroscopyx.github.io/"
+SITE_URL = "https://plasmapy.github.io/spectroscopyx/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
-BASE_URL = "https://spectroscopyx.github.io/"
+BASE_URL = "https://plasmapy.github.io/spectroscopyx/"
 PROJECT_REPOSITORY = "https://github.com/PlasmaPy/SpectroscoPyx"
 BLOG_DESCRIPTION = "Webpage for SpectroscoPyx."  # (translatable)
 
